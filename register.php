b1f2f3aa1074b1886f9eaebe672a50dcdfe64857
SamritUddam <samrituddam20@gmail.com>
2022-07-09 13:16:51 +0700

    Update

--- register.php
+++ register.php
@@ -10,7 +10,7 @@
     <h2>Register</h2>
 </div>
 
-<form method="post" action="register.php">
+<form method="post" action="" enctype="multipart/form-data">
     <?php include('errors.php'); ?>
     <div class="input-group">
         <label>Username</label>
@@ -21,12 +21,38 @@
         <input type="email" name="email" value="<?php echo $email; ?>">
     </div>
     <div class="input-group">
+        <label>Date Of Birth</label>
+        <input type="date" name="date_of_birth">
+
+    </div>
+
+    <div class="input-group">
+        <label>Phone</label>
+        <input type="number" name="phone">
+
+    </div>
+
+    <div class="input-group">
+        <label>Gender</label>
+        <select name="gender" id="gender" style="width:200px;">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Other">Other</option>
+        </select>
+
+    </div>
+
+    <div class="input-group">
         <label>Password</label>
         <input type="password" name="password_1">
     </div>
     <div class="input-group">
         <label>Confirm password</label>
         <input type="password" name="password_2">
+    </div>
+    <div class="input-group">
+        <label>Image</label>
+        <input type="file" name="image">
     </div>
     <div class="input-group">
         <button type="submit" class="btn" name="reg_user">Register</button>
--- server.php
+++ server.php
@@ -7,20 +7,27 @@ $email    = "";
 $errors = array();
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', 'root', 'registration');
+$db = mysqli_connect('localhost', 'root', 'root', 'login_form');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
+
     // receive all input values from the form
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $email = mysqli_real_escape_string($db, $_POST['email']);
+    $date_of_birth = mysqli_real_escape_string($db, $_POST['date_of_birth']);
+    $gender = mysqli_real_escape_string($db, $_POST['gender']);
+    $phone = mysqli_real_escape_string($db, $_POST['phone']);
     $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
     $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+    $image = mysqli_real_escape_string($db, "data.jpg");
 
     // form validation: ensure that the form is correctly filled ...
     // by adding (array_push()) corresponding error unto $errors array
     if (empty($username)) { array_push($errors, "Username is required"); }
     if (empty($email)) { array_push($errors, "Email is required"); }
+    if (empty($date_of_birth)) { array_push($errors, "Date Of Birth is required"); }
+    if (empty($gender)) { array_push($errors, "Gender Of Birth is required"); }
     if (empty($password_1)) { array_push($errors, "Password is required"); }
     if ($password_1 != $password_2) {
         array_push($errors, "The two passwords do not match");
@@ -46,11 +53,41 @@ if (isset($_POST['reg_user'])) {
     if (count($errors) == 0) {
         $password = md5($password_1);//encrypt the password before saving in the database
 
-        $query = "INSERT INTO users (username, email, password) 
-  			  VALUES('$username', '$email', '$password')";
+        $query = "INSERT INTO users (username, email, password, date_of_birth, phone, gender, image) 
+  			  VALUES('$username', '$email', '$password', '$date_of_birth','$phone', '$gender', '$image')";
         mysqli_query($db, $query);
         $_SESSION['username'] = $username;
         $_SESSION['success'] = "You are now logged in";
         header('location: index.php');
     }
 }
+
+// ...
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            header('location: index.php');
+        }else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+}
+
+?>
